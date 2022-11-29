28e35d5adfb4d641e54d3d0934914c2bd1556b51
Achille kabasele <pepexykabasele@gmail.com>
2022-11-29 16:05:00 +0100

    update

--- frontend/KivyApp/main.py
+++ frontend/KivyApp/main.py
@@ -11,16 +11,26 @@ from kivy.properties import StringProperty, ObjectProperty
 from kivy.uix.widget import Widget
 from kivy.lang import Builder
 from kivymd.app import MDApp
+from kivymd.uix.list import OneLineIconListItem
+
 
 
 #recycle view for home screen
+
+class DonneEtudianList(OneLineIconListItem):
+    pass
+
 class MyRecycleView(RecycleView):
+    text_title = ObjectProperty(None)
+    
     def __init__(self, **kwargs):
         super(MyRecycleView, self).__init__(**kwargs)
+        
         self.load_data()
         Clock.schedule_interval(self.load_data, 1)
 
     def load_data(self, *args):
+        #card_id = root.ids.card_id
         store = requests.get('http://127.0.0.1:8000/api').json()
 
         list_data = []
@@ -28,6 +38,7 @@ class MyRecycleView(RecycleView):
             list_data.append({'text': item['name']})
 
         self.data = list_data
+        self.text_title = self.data
 
 
 class AddMessage(Widget):
@@ -43,6 +54,7 @@ class AddMessage(Widget):
         print("voir ",post)
 
         self.input = ''
+
 
 
 class HomeScreen(Screen):
